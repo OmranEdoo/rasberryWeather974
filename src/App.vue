e6bd692256435df1b2076b2922cb19c6f4be4456
omranedoo <omranedoo@gmail.com>
2023-02-13 15:29:09 +0100

    ajout dépendances
    test

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <template>
-  <NavBar></NavBar>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import NavBar from './components/NavBar.vue'
 
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
-    NavBar
+    HelloWorld
   }
 }
 </script>
