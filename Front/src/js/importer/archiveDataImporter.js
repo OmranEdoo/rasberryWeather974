545e1d2f3833499403e164b896a678f859956cd1
zumbalove974 <omranedoo@gmail.com>
2023-03-16 01:57:34 +0100

    changement du font archive + ajout de l'url comme attribut

--- Front/src/js/importer/archiveDataImporter.js
+++ Front/src/js/importer/archiveDataImporter.js
@@ -1,23 +1,18 @@
-export const getArchiveData = async function getArchiveData(periode, feature) {
-    return fetch('http://piensg027:3000/archive/' + periode + '/' + feature, {
+export const getArchiveData = async function getArchiveData(url, periode, feature) {
+    return fetch(url + periode + '/' + feature, { //'http://piensg027:3000/archive/'
         method: 'GET'
     })
         .then(response => response.json())
         .then(response => {
-            console.log("uuuuu");
 
             switch (feature) {
                 case 'lum':
-                    console.log(response.measurements.lum);
                     return response.measurements.lum;
                 case 'temp':
-                    console.log("yyy");
                     return response.measurements.temp;
                 case 'hum':
-                    console.log(response.measurements.hum);
                     return response.measurements.hum;
                 case 'pre':
-                    console.log(response.measurements.pre);
                     return response.measurements.pre;
                 default:
                     return "";
