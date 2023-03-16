7cabcb8c5300f3120ede51fe9640f25eef2eadc9
LinaSaba <linasaba.18@gmail.com>
2023-03-16 13:40:27 +0100

    archive working but not the list of measures

--- Back/routes/archive.js
+++ Back/routes/archive.js
@@ -13,7 +13,6 @@ function createInfluxClient() {
     return influx;
 }
 
-
 router.get('/:period/:param', async function (req, res, next) {
     let param = req.params.param;
     let endDatetime = req.params.endDatetime;
@@ -27,19 +26,18 @@ router.get('/:period/:param', async function (req, res, next) {
     console.log("param : ", param)
     let period = req.params.period;
     console.log("period : ", period)
-
     let gps = influx.query(
         `SELECT * FROM gps GROUP BY * ORDER BY DESC LIMIT 10`
     );
     //console.log('gps', gps)
-    function test() {
+    function GPS() {
         return gps.then(result => {
             const lat = result[0].latitude;
             const long = result[0].longitude;
             return [lat, long];
         })
     }
-    let coord = await test();
+    let coord = GPS();
     valeurs = {
         id: 30,
         name: 'RaspberryWeather974',
@@ -52,90 +50,105 @@ router.get('/:period/:param', async function (req, res, next) {
 
     }
 
-    valeurs.measurements[param] = {
-        name: results[index].name,
-        desc: results[index].name,
-        unit: results[index].unit,
-    }
-    
-    let allValue = [];
-    let allTime = [];
-    let value = [];
-    let time = [];
-    let L = [];
-    let result;
-
-    let period_date = new Date();
-    let step;
-    let stepping;
-    if (period == "day") {
-        console.log("------day case------");
-        period_date.setHours(endDatetime.getHours() - 24);
-        console.log("date_day : ", period_date);
-        stepping = 24;
-        let a = period_date;
-        for (let i = 1; i < stepping+1; i++){
-            console.log(i)
-            console.log("date_dayà : ", period_date);
-            console.log("AA",a)
-            a.setHours(a.getHours()+1);
-            console.log("EUH",a)
-            result = influx.query(
-                `SELECT mean(value) as mean FROM ${param} WHERE time > '${period_date.toISOString()}' AND time < '${a.toISOString()}'`
-            );
-            await result.then(mean=>{
-                allValue.push(mean);
-                allTime.push(period_date)
-            })
+    let namequery = influx.query(
+        `select * from ${param} GROUP BY * ORDER BY DESC LIMIT 1`
+    );
+    namequery.then(results => {
+        for (let index = 0; index < results.length; index++) {
+            valeurs.measurements[param] = {
+                name: results[index].name,
+                desc: results[index].desc,
+                unit: results[index].unit,
+            }
         }
-        valeurs.measurements[param].values = allValue;
-        valeurs.measurements[param].times = allTime;
-        
-    }
-    else if (period == "week") {
-        console.log("------week case------");
-        period_date.setHours(endDatetime.getHours() - 7*24);
-        console.log("date_week : ", period_date);
-        stepping = 14;
-        step = (endDatetime - period_date) / stepping;
-        console.log("step_week : ", step);
-    }
-    else if (period == "month") {
-        console.log("------month case------");
-        period_date.setMonth(endDatetime.getMonth() - 1);
-        console.log("date_month : ", period_date);
-        stepping = 30;
-        step = (endDatetime - period_date) / stepping;
-        console.log("step_month : ", step);
-    }
-    else if (period == "year") {
-        console.log("------year case------");
-        period_date.setFullYear(endDatetime.getFullYear() - 1);
-        console.log("date_year : ", period_date);
-        stepping = 52;
-        step = (endDatetime - period_date) / stepping;
-        console.log("step_year : ", step);
-    }
-    
-    
-    // await result.then(results => {
-    //     for (let index = 0; index < results.length; index++) {
-    //         console.log("VALEUR",results[index])
-    //         // allValue.push(parseFloat(results[index].value))
-    //         // allTime.push(results[index].date)
-    //         // valeurs.measurements[param] = {
-    //         //     name: results[index].name,
-    //         //     desc: results[index].name,
-    //         //     unit: results[index].unit,
-    //         // }
-    //     }
-                
-    //     // 
-    // })
-    
 
-    res.json(valeurs)
-}
-)
+        let allValue = [];
+        let allTime = [];
+        let value = [];
+        let time = [];
+        let L = [];
+        let result;
+
+        let period_date = new Date();
+        let step;
+        let stepping;
+        if (period == "day") {
+            console.log("------day case------");
+            period_date.setHours(endDatetime.getHours() - 24);
+            console.log("date_day : ", period_date);
+            stepping = 24;
+            let a = period_date;
+            for (let i = 1; i < stepping + 1; i++) {
+                console.log(i)
+                console.log("date_dayà : ", period_date);
+                console.log("AA", a)
+                a.setHours(a.getHours() + 1);
+                console.log("EUH", a)
+                result = influx.query(
+                    `SELECT mean(value) as mean FROM ${param} WHERE time > '${period_date.toISOString()}' AND time < '${a.toISOString()}'`
+                );
+                result.then(r => {
+                    allValue.push(r.mean);
+                    console.log("value", r)
+
+                    allTime.push(period_date)
+                    console.log("perido", period_date)
+
+                })
+            }
+            valeurs.measurements[param].values = allValue;
+            console.log("values List", allValue)
+            valeurs.measurements[param].times = allTime;
+            console.log("times List", allTime)
+
+
+        }
+        else if (period == "week") {
+            console.log("------week case------");
+            period_date.setHours(endDatetime.getHours() - 7 * 24);
+            console.log("date_week : ", period_date);
+            stepping = 14;
+            step = (endDatetime - period_date) / stepping;
+            console.log("step_week : ", step);
+        }
+        else if (period == "month") {
+            console.log("------month case------");
+            period_date.setMonth(endDatetime.getMonth() - 1);
+            console.log("date_month : ", period_date);
+            stepping = 30;
+            step = (endDatetime - period_date) / stepping;
+            console.log("step_month : ", step);
+        }
+        else if (period == "year") {
+            console.log("------year case------");
+            period_date.setFullYear(endDatetime.getFullYear() - 1);
+            console.log("date_year : ", period_date);
+            stepping = 52;
+            step = (endDatetime - period_date) / stepping;
+            console.log("step_year : ", step);
+        }
+
+
+        // await result.then(results => {
+        //     for (let index = 0; index < results.length; index++) {
+        //         console.log("VALEUR",results[index])
+        //         // allValue.push(parseFloat(results[index].value))
+        //         // allTime.push(results[index].date)
+        //         // valeurs.measurements[param] = {
+        //         //     name: results[index].name,
+        //         //     desc: results[index].name,
+        //         //     unit: results[index].unit,
+        //         // }
+        //     }
+
+        //     // 
+        // })
+
+
+        res.json(valeurs)
+    }
+    )
+
+})
 
 module.exports = router;
