48dc4273a1f5478d17163e94598d20a670e559c0
omranedoo <omranedoo@gmail.com>
2023-03-16 12:56:24 +0100

    home page and about page front ok

--- Front/src/pages/Home/App.vue
+++ Front/src/pages/Home/App.vue
@@ -1,11 +1,40 @@
 <template>
   <div id="app">
     <NavBar actual-page="home" />
+    <div class="containerRow">
+      <MDBCard class="card">
+        <a v-mdb-ripple="{ color: 'light' }">
+          <MDBCardImg src="https://mdbootstrap.com/img/new/standard/nature/184.webp" top alt="..." />
+        </a>
+        <MDBCardBody>
+          <MDBCardTitle>Live Data</MDBCardTitle>
+          <MDBCardText>
+            Look at the data captured by the rasberrypy probes in real time !
+          </MDBCardText>
+          <MDBBtn tag="a" href="live" color="primary">Click</MDBBtn>
+        </MDBCardBody>
+      </MDBCard>
+
+      <MDBCard class="card">
+        <a v-mdb-ripple="{ color: 'light' }">
+          <MDBCardImg src="https://mdbootstrap.com/img/new/standard/nature/191.webp" top alt="..." />
+        </a>
+        <MDBCardBody>
+          <MDBCardTitle>Archive Data</MDBCardTitle>
+          <MDBCardText>
+            See older data taken by the rasberry's !
+          </MDBCardText>
+          <MDBBtn tag="a" href="archive" color="primary">Click</MDBBtn>
+        </MDBCardBody>
+      </MDBCard>
+    </div>
     <FooterBar></FooterBar>
   </div>
 </template>
 
 <script>
+import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardImg, MDBBtn, mdbRipple } from "mdb-vue-ui-kit";
+
 import NavBar from './../../components/NavBar.vue'
 import FooterBar from './../../components/FooterBar.vue'
 
@@ -14,7 +43,16 @@ export default {
   name: 'App',
   components: {
     NavBar,
-    FooterBar
+    FooterBar,
+    MDBCard,
+    MDBCardBody,
+    MDBCardTitle,
+    MDBCardText,
+    MDBCardImg,
+    MDBBtn
+  },
+  directives: {
+    mdbRipple
   }
 }
 </script>
@@ -27,5 +65,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin: 0;
+}
+
+.containerRow {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+}
+
+.card {
+  width: 48vw;
 }
 </style>
